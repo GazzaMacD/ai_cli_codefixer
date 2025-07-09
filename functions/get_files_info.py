b82f1111bba086d2fbf94b9524324629b808297a
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-07-09 15:11:12 +0200

    Start building basic functions to run with the ai agent

---
+++ functions/get_files_info.py
@@ -0,0 +1,26 @@
+# get_files_info.py
+import os
+
+
+def get_files_info(working_directory, directory=None):
+    """One level deep check for a directory to get basic information about the files and dirs in it"""
+    if not directory or not isinstance(directory, str):
+        return "Error: The second argument to get_files_info is required and must be a string"
+    path = os.path.abspath(os.path.join(working_directory, directory))
+    print("path ->", path)
+    if working_directory not in path:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(path):
+        return f'Error: "{directory}" is not a directory'
+    info = ""
+    for content in os.listdir(path):
+        path_to_content = os.path.join(path, content)
+        if os.path.isfile(path_to_content):
+            info += f"- {content}: file_size={os.path.getsize(path_to_content)}, is_dir=False\n"
+        else:
+            info += f"- {content}: file_size={os.path.getsize(path_to_content)}, is_dir=True\n"
+    return info
+
+
+def get_file_content(working_directory, file_path):
+    pass
---
+++ tests.py
@@ -0,0 +1,17 @@
+# tests.py
+
+import unittest
+
+from functions.get_files_info import get_files_info
+
+
+class TestFunctions(unittest.TestCase):
+    def test_smoke(self):
+        print(get_files_info("calculator", "."))
+        print(get_files_info("calculator", "pkg"))
+        print(get_files_info("calculator", "/bin"))
+        print(get_files_info("calculator", "../"))
+
+
+if __name__ == "__main__":
+    unittest.main()
