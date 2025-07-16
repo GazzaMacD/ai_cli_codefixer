26a386e45d84fabbff133637ea76eb51baa4e12a
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-07-16 06:29:01 +0200

    Refactor main

--- main.py
+++ main.py
@@ -5,27 +5,40 @@ from dotenv import load_dotenv
 from google.genai import Client, types as genai_types
 
 from settings import available_functions, system_prompt
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = Client(api_key=api_key)
+from call_function import call_function
 
 
 def main():
+    load_dotenv()
     user_prompt = None
-    flag1 = None
-    if len(sys.argv) < 2:
-        print("Error: A text query for the LLM is required after main.py")
-        exit(1)
-    user_prompt = sys.argv[1]
+    verbose = "--verbose" in sys.argv
+    args = []
 
-    if len(sys.argv) == 3:
-        flag1 = sys.argv[2]
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = Client(api_key=api_key)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
     messages = [
         genai_types.Content(role="user", parts=[genai_types.Part(text=user_prompt)]),
     ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
@@ -33,16 +46,27 @@ def main():
             tools=[available_functions], system_instruction=system_prompt
         ),
     )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    if flag1 and flag1 == "--verbose":
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     if not response.function_calls:
-        print(response.text)
+        return response.text
 
+    function_responses = []
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
 
 
 if __name__ == "__main__":
